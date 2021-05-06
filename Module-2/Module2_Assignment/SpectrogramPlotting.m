f043311198f26667b3dcd621d4e95bd221d0d04c
Reland <rubeneland@hotmail.com>
2021-05-06 09:56:45 +0200

    commit

--- Module-2/Module2_Assignment/SpectrogramPlotting.m
+++ Module-2/Module2_Assignment/SpectrogramPlotting.m
@@ -5,8 +5,8 @@ L= (20/1000)*8000;
 % "ov": Length of the overlap (in samples)
 ov= L*0.5;
 
-% WORD 1: GIJP
-y=dataPieter(:,1);
+% WORD 1: BAK
+y=DataRuben(:,1);
 y_norm= y - sum(y)/length(y);
 % devide by the max abs value
 y_norm= y_norm / max(abs(y_norm));
@@ -22,14 +22,14 @@ f0 = FindPitch(y_nosilence,Fs,L,ov);
 figure;
 subplot(2,2,1);
 imagesc(t,f,10*log10(ps)); axis xy;
-title('GIJP');
+title('BAK');
 hold on;
 plot(t_vec,f0,'Color','red','LineWidth',2);
 plot(t_vec,frmtns,'Color','black','LineWidth',1.5);
 xlabel('Time(s)');ylabel('Frequency(Hz)'); hold off;
 
-% WORD 2: STOP
-y=dataPieter(:,11);
+% WORD 2: Gij
+y=DataRuben(:,11);
 y_norm= y - sum(y)/length(y);
 % devide by the max abs value
 y_norm= y_norm / max(abs(y_norm));
@@ -44,14 +44,14 @@ f0 = FindPitch(y_nosilence,Fs,L,ov);
 [~,f,t,ps]=spectrogram(y_nosilence, L,ov,L,Fs,'yaxis');
 subplot(2,2,2);
 imagesc(t,f,10*log10(ps)); axis xy;
-title('STOP');
+title('GIJ');
 hold on;
 plot(t_vec,f0,'Color','red','LineWidth',2);
 plot(t_vec,frmtns,'Color','black','LineWidth',1.5);
 xlabel('Time(s)');ylabel('Frequency(Hz)'); hold off;
 
-% WORD 3: BAK
-y=dataPieter(:,21);
+% WORD 3: STOP
+y=DataRuben(:,21);
 y_norm= y - sum(y)/length(y);
 % devide by the max abs value
 y_norm= y_norm / max(abs(y_norm));
@@ -66,14 +66,14 @@ f0 = FindPitch(y_nosilence,Fs,L,ov);
 [~,f,t,ps]=spectrogram(y_nosilence, L,ov,L,Fs,'yaxis');
 subplot(2,2,3);
 imagesc(t,f,10*log10(ps)); axis xy;
-title('BAK');
+title('STOP');
 hold on;
 plot(t_vec,f0,'Color','red','LineWidth',2);
 plot(t_vec,frmtns,'Color','black','LineWidth',1.5);
 xlabel('Time(s)');ylabel('Frequency(Hz)'); hold off;
 
-% WORD 4: STUUR
-y=dataPieter(:,31);
+% WORD 4: UUR
+y=DataRuben(:,31);
 y_norm= y - sum(y)/length(y);
 % devide by the max abs value
 y_norm= y_norm / max(abs(y_norm));
@@ -88,7 +88,7 @@ f0 = FindPitch(y_nosilence,Fs,L,ov);
 [~,f,t,ps]=spectrogram(y_nosilence, L,ov,L,Fs,'yaxis');
 subplot(2,2,4);
 imagesc(t,f,10*log10(ps)); axis xy;
-title('STUUR');
+title('UUR');
 hold on;
 plot(t_vec,f0,'Color','red','LineWidth',2);
 plot(t_vec,frmtns,'Color','black','LineWidth',1.5);
