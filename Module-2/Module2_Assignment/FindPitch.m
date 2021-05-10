460b0f5306829409b5f2679cc62b6919678a62e8
PieterGo <pieter.gommers@hotmail.com>
2021-05-10 14:20:04 +0200

    Vervelend dit

--- Module-2/Module2_Assignment/FindPitch.m
+++ Module-2/Module2_Assignment/FindPitch.m
@@ -1,3 +1,8 @@
+% Group A5 - EPO4 2021
+% Authors: Ruben Eland & Pieter Gommers
+% Description:
+% This function finds the pitch (fundamental frequency) for a given input y.
+
 function f0= FindPitch(y,Fs,L,ov)
     f0 = pitch(y, Fs, 'WindowLength', L, 'OverlapLength', ov, 'Method', 'CEP');
 end
